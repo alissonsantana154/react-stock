406e249e4cda79579d4a30d0a783781201d5d7b7
Alisson Santana <alissonsantana16608@gmail.com>
2023-11-21 10:24:16 -0300

    cadastro de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
+import { StockContextProvider } from "./contexts/StockContext"
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
-  )
+    <StockContextProvider>
+      <RouterProvider router={router} />
+    </StockContextProvider>
+  )  
 }
 
 export default App
